3446a8d647faa5c6b85806c89ba32c6c67b6de94
wilddog1979 <ferenc.kemeny79@gmail.com>
2024-11-23 22:47:24 +0100

    unused import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
-import { AppComponent } from "./app.component";
-
 export const routes: Routes = [
   { path: '', component: LandingPageComponent }
 ];
