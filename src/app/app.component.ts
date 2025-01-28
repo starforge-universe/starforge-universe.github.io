2a5d3b27423047c8b7375ed0134c3d29560ff87f
wilddog1979 <ferenc.kemeny79@gmail.com>
2025-01-28 22:19:39 +0100

    initial version of the page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [
+      RouterOutlet,
+      HeaderComponent,
+      SidebarComponent,
+      ContentComponent,
+      FooterComponent
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     standalone: true
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   imports: [
     AppRoutingModule,
     BrowserModule,
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   bootstrap: [AppComponent]
 })
