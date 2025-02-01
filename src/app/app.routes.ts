fb22d039e1eec4a02b31ebc16a9ca386dda3c90d
wilddog1979 <ferenc.kemeny79@gmail.com>
2025-02-01 11:01:12 +0100

    component folder designed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 import { MsalGuard, MsalRedirectComponent } from "@azure/msal-angular";
 
