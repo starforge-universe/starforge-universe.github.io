90951ef4bd2c5b65c7c0ea86482d45ffb2552652
Star Forge Worker <star.forge.worker@gmail.com>
2025-03-17 22:42:44 +0100

    # Conflicts: #	.github/dependabot.yaml #	package-lock.json #	package.json #	src/app/app.module.ts #	src/app/components/app.component.html #	src/app/components/app.component.ts #	src/styles.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './components/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   imports: [
     AppRoutingModule,
     BrowserModule,
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   bootstrap: [AppComponent]
 })
