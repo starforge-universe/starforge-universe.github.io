bf89f87b4ac5bee2a5a283cb56965a0d494a6b18
Star Forge Worker <star.forge.worker@gmail.com>
2025-03-17 22:46:35 +0100

    # Conflicts: #	.github/dependabot.yaml #	angular.json #	package-lock.json #	package.json #	src/app/app.routes.ts #	src/app/components/app.component.html #	src/app/components/landing-page/landing-page.component.html #	src/app/components/landing-page/landing-page.component.spec.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { StarforgeSurveySwaggerComponent } from './components/starforge-survey-swagger/starforge-survey-swagger.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
+  { path: 'starforge-survey-swagger', component: StarforgeSurveySwaggerComponent }
   { path: '**', component: PageNotFoundComponent }
 ];
 
